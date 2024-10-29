306a3e7b4f290dd1513da13faab7001f48d3ec81
linux bakaraw <bacalso.ton2x@gmail.com>
2024-10-29 19:22:51 +0800

    refactor: made a separate component for the start, pause, and stop btns

---
+++ src/components/timerControlBtns.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import RectDOM from 'react-dom';
+
+type TimerControls = {
+  startTimer: () => void;
+  pauseTimer: () => void;
+  stopTimer: () => void;
+}
+
+export const TimerControlBtns = (params: TimerControls) => {
+  return (
+    <div className='col-span-2 grid grid-cols-3'>
+      <div><button onClick={params.startTimer}>Start</button></div>
+      <div><button onClick={params.pauseTimer}>Pause</button></div>
+      <div><button onClick={params.stopTimer}>Stop</button></div>
+    </div>
+  );
+}
