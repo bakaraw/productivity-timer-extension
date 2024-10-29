e11a992646b5967423a69eb244bb0f0bae2a424b
linux bakaraw <bacalso.ton2x@gmail.com>
2024-10-29 16:15:39 +0800

    add: unit test for App and useTimer

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { Port } from './types/testingTypes';
 
-test('renders learn react link', () => {
+const mockChromeRuntimeConnect = (): Port['onMessage'] => {
+  const onMessageMock = {
+    addListener: jest.fn(),
+    removeListener: jest.fn(),
+    hasListener: jest.fn(),
+  };
+  const portMock: Port = {
+    name: 'timerUpdates',
+    onMessage: onMessageMock,
+    onDisconnect: {
+      addListener: jest.fn(),
+      removeListener: jest.fn(),
+    },
+    postMessage: jest.fn(),
+    disconnect: jest.fn(),
+  };
+
+  (global.chrome.runtime.connect as jest.Mock) = jest.fn(() => portMock);
+  return onMessageMock;
+};
+
+test('renders Timer link and connects to Chrome runtime', () => {
+  const onMessageMock = mockChromeRuntimeConnect();
+  const addListenerSpy = jest.spyOn(onMessageMock, 'addListener');
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId(/app-1/i);
   expect(linkElement).toBeInTheDocument();
+
+  expect(addListenerSpy).toHaveBeenCalled();
 });
