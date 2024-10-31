7eaffef1549f1fd9f4083ab9c5c7babf915d4329
linux bakaraw <bacalso.ton2x@gmail.com>
2024-10-31 09:59:11 +0800

    add: dashboard and useTimeControls now accepts restDuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { TimerControllerUI } from './components/timerControllerUI';
+import { Dashboard } from './pages/dashboard';
 
 function App() {
   return (
     <div className="App" data-testid="app-1">
       <header className="App-header">
         <div ></div>
-        <TimerControllerUI />
+        <Dashboard />
       </header>
     </div>
   );
