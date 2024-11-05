b44d0e5182b9d11c2f067932b038004334e81869
linux bakaraw <bacalso.ton2x@gmail.com>
2024-11-05 18:15:32 +0800

    fix: timer input

--- src/components/timerDisplay.tsx
+++ src/components/timerDisplay.tsx
@@ -12,17 +12,8 @@ type TimerTextProps = {
 export const TimerDisplay = (params: TimerTextProps) => {
   return (
     <div className='grid grid-cols-2 gap-2'>
-      <NumberInput value={params.timeLeft.minutes} onChange={(value) => params.changeMinutes(value)} min={0} max={999} />
-      <NumberInput value={params.timeLeft.seconds} onChange={(value) => params.changeSeconds(value)} min={0} max={999} />
+      <NumberInput value={params.timeLeft.minutes} onChange={(value) => params.changeMinutes(value)} min={0} max={300} />
+      <NumberInput value={params.timeLeft.seconds} onChange={(value) => params.changeSeconds(value)} min={0} max={60} />
     </div>
   );
-  //<div className="grid grid-cols-2 gap-2">
-  //  <div><button onClick={() => params.changeMinutes(true)}>up</button></div>
-  //  <div><button onClick={() => params.changeSeconds(true)}>up</button></div>
-  //  <NumberInput value={reps} onChange={(value) => setReps(value)} min={1} max={100} />
-  //  <div>{params.timeLeft.minutes}</div>
-  //  <div>{params.timeLeft.seconds}</div>
-  //  <div><button onClick={() => params.changeMinutes(false)}>down</button></div>
-  //  <div><button onClick={() => params.changeSeconds(false)}>down</button></div>
-  //</div>
 }
